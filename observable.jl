9451496c37af65ea6d5c16a86ee5d04b6fb23fef
crstnbr <mail@carstenbauer.eu>
2017-01-31 21:23:15 +0100

    observable type can now handle and save (hdf5) arbitrary real and complex matrices, vectors, numbers

--- observable.jl
+++ observable.jl
@@ -1,24 +1,17 @@
 import HDF5
 
-type observable{T<:Union{Number,Array}}
+type observable{T<:Union{Number,AbstractArray}}
   name::String
   count::Int
   timeseries::Array{T, 1}
 
-  function observable(name::String,alloc::Int)
-    if T <: Number
-      new(name,0,zeros(T,alloc))
-    elseif T <: Array
-      new(name,0,Array{T, 1}(alloc))
-    end
-  end
-
+  observable(name::String,alloc::Int) = new(name,0,Array{T, 1}(alloc))
   observable(name::String) = observable{T}(name,256)
 end
 
 
 function add_value{T}(obs::observable{T}, value::T)
-  if size(obs.timeseries)[1] < obs.count+1
+  if length(obs.timeseries) < obs.count+1
     # backup if allocation wasn't correct
     push!(obs.timeseries,copy(value))
   end
@@ -26,8 +19,18 @@ function add_value{T}(obs::observable{T}, value::T)
   obs.count += 1
 end
 
+
 function obs2hdf5{T}(filename::String, obs::observable{T})
-  HDF5.h5write(filename, "simulation/results/" * obs.name * "/count", obs.count - 1)
-  HDF5.h5write(filename, "simulation/results/" * obs.name * "/timeseries", obs.timeseries)
-  HDF5.h5write(filename, "simulation/results/" * obs.name * "/mean", mean(obs.timeseries))
+  HDF5.h5write(filename, "simulation/results/" * obs.name * "/count", obs.count)
+  timeseries_matrix = cat(ndims(obs.timeseries[1])+1,obs.timeseries[1:obs.count]...)
+  nd = ndims(timeseries_matrix)
+  if eltype(timeseries_matrix)<:Real
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/timeseries", timeseries_matrix)
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/mean", squeeze(mean(timeseries_matrix,nd),nd))
+  else
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/timeseries_real", real(timeseries_matrix))
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/timeseries_imag", imag(timeseries_matrix))
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/mean_real", squeeze(mean(real(timeseries_matrix),nd),nd))
+    HDF5.h5write(filename, "simulation/results/" * obs.name * "/mean_imag", squeeze(mean(imag(timeseries_matrix),nd),nd))
+  end
 end
