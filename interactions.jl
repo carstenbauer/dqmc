a7eb1ba1a636075d26a42f7e39fb4bb95be4a689
crstnbr <mail@carstenbauer.eu>
2017-10-27 19:54:20 +0200

    store norm op

--- interactions.jl
+++ interactions.jl
@@ -6,8 +6,9 @@ function interaction_matrix_exp(p::Parameters, l::Lattice, slice::Int, power::Fl
   S = blockview(l, eV, 1, 2)
   R = blockview(l, eV, 1, 4)
   @simd for i in 1:l.sites
-    sh = sinh(p.lambda * p.delta_tau * norm(p.hsfield[:,i,slice]))/norm(p.hsfield[:,i,slice])
-    C[i,i] = cosh(p.lambda * p.delta_tau * norm(p.hsfield[:,i,slice]))
+    n = norm(p.hsfield[:,i,slice])
+    sh = sinh(p.lambda * p.delta_tau * n)/n
+    C[i,i] = cosh(p.lambda * p.delta_tau * n)
     S[i,i] = (im * p.hsfield[2,i,slice] - p.hsfield[1,i,slice]) * power * sh
     R[i,i] = (-p.hsfield[3,i,slice]) * power * sh
   end
@@ -29,29 +30,17 @@ function interaction_matrix_exp(p::Parameters, l::Lattice, slice::Int, power::Fl
   return eV
 end
 
-
 blockview{T<:Number}(l::Lattice, A::Matrix{T}, row::Int, col::Int) = view(A, (row-1)*l.sites+1:row*l.sites, (col-1)*l.sites+1:col*l.sites)
 function blockreplace!{T<:Number}(l::Lattice, A::Matrix{T}, row::Int, col::Int, B::Union{Matrix{T},SubArray{T,2}})
   @inbounds A[(row-1)*l.sites+1:row*l.sites, (col-1)*l.sites+1:col*l.sites] = B
   nothing
 end
 
-
 # calculate p.flv x p.flv (4x4 for O(3) model) interaction matrix exponential for given op
 function interaction_matrix_exp_op(p::Parameters, l::Lattice, op::Vector{Float64}, power::Float64=1.)
-  sh = power * sinh(p.lambda * p.delta_tau*norm(op))/norm(op)
-  Cii = cosh(p.lambda * p.delta_tau*norm(op))
-  Sii = (im * op[2] - op[1]) * sh
-  Rii = (-op[3]) * sh
-
-  return [Cii Sii 0 Rii; conj(Sii) Cii -Rii 0; 0 -Rii Cii conj(Sii); Rii 0 Sii Cii]
-end
-# Small optimization left to do here.
-
-# calculate p.flv x p.flv (4x4 for O(3) model) interaction matrix exponential for given op
-function interaction_matrix_exp_op2(p::Parameters, l::Lattice, op::Vector{Float64}, power::Float64=1.)
-  sh = power * sinh(p.lambda * p.delta_tau*norm(op))/norm(op)
-  Cii = cosh(p.lambda * p.delta_tau*norm(op))
+  n = norm(op)
+  sh = power * sinh(p.lambda * p.delta_tau*n)/n
+  Cii = cosh(p.lambda * p.delta_tau*n)
   Sii = (im * op[2] - op[1]) * sh
   Rii = (-op[3]) * sh
 
