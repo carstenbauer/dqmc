0dac353d981e8c3b5555b59ce6da1c2e1c2f1055
Carsten Bauer <crstnbr@gmail.com>
2018-04-10 12:13:47 +0200

    CB vs L test

--- tests/CB_vs_L.jl
+++ tests/CB_vs_L.jl
@@ -1,19 +1,36 @@
-using Plots; pyplot()
-using DataFrames, StatPlots
+using Plots;
+using DataFrames, StatPlots, BenchmarkTools, NamedTuples
 default(:framestyle, :box)
 
 input = "CB_vs_L.in.xml"
-include("../dqmc_framework.jl")
+include("../live.jl")
 
-p = Params()
-p.output_file = "CB_vs_L.out.h5.running"
-xml2parameters!(p, input)
+df = DataFrame(L=Int[], CB=String[], 
+	multiply_B=Float64[], greens=Float64[], build_stack=Float64[])
 
-for (l,L) in enumerate([2,4,6,8])
+for (Li,L) in enumerate([2,3,4,5])
+	println("------------------")
+	@show L
+
 	p.L = L
-	p.lattice_file = ""
+	p.lattice_file = "C:\\Users\\carsten\\Desktop\\sciebo\\lattices\\square_L_$(p.L)_W_$(p.L).xml"
 	mc = DQMC(p)
 	init!(mc)
+	global g = copy(mc.s.greens)
+	M = copy(g)
 
-	t = @elapsed 
+	tB = @belapsed multiply_slice_matrix_left!($mc, 1, $M) setup=(M=copy(g))
+	tgreens = @belapsed measure_greens($mc)
+	tstack = @belapsed build_stack($mc)
+
+	push!(df, [L, string(cbtype(mc)), tB, tgreens, tstack])
 end
+
+pyplot(size=(800,800))
+
+@df df plot(:L, [:multiply_B, :greens], group=(:CB),
+	marker=:circle,
+	xlab="L",
+	ylab="elapsed time",
+	layout=grid(2,1, heights=(.5,.5))
+)
