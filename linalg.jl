2fa70183c2c47f64ec3d49efd9d0da28255cfdec
Carsten Bauer <crstnbr@gmail.com>
2018-11-22 08:54:41 +0100

    (much) more efficient mul!(dense, dense, sparse)

--- linalg.jl
+++ linalg.jl
@@ -64,8 +64,12 @@ function mul!(C::StridedMatrix, X::StridedMatrix, A::SparseMatrixCSC)
     fill!(C, zero(eltype(C)))
     rowval = A.rowval
     nzval = A.nzval
-    @inbounds for multivec_row=1:mX, col = 1:A.n, k=A.colptr[col]:(A.colptr[col+1]-1)
-        C[multivec_row, col] += X[multivec_row, rowval[k]] * nzval[k]
+    @inbounds for  col = 1:A.n, k=A.colptr[col]:(A.colptr[col+1]-1)
+        ki=rowval[k]
+        kv=nzval[k]
+        for multivec_row=1:mX
+            C[multivec_row, col] += X[multivec_row, ki] * kv
+        end
     end
     C
 end
