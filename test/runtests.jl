41e19ae036580ccfd84fc42d978083b679e70025
Carsten Bauer <crstnbr@gmail.com>
2019-02-19 10:51:01 +0100

    hide dqmc init ouput in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,53 @@
 using Test
-include("../dqmc_framework.jl") # to be replaced by using DQMC or similar
+include("../src/dqmc_framework.jl") # to be replaced by using DQMC or similar
 
 function mc_from_inxml(inxml::AbstractString)
+    if SUPPRESS_DQMC_INIT_OUTPUT
+        old_stdout = stdout
+        (rp, wp) = redirect_stdout()
+    end
+
     @assert isfile(inxml)
     p = Params()
     p.output_file = "$(inxml[1:end-7]).out.h5.running"
     xml2parameters!(p, inxml)
     mc = DQMC(p)
     init!(mc)
-    mc
+
+    if SUPPRESS_DQMC_INIT_OUTPUT
+        redirect_stdout(old_stdout)
+        close(wp); close(rp);
+    end
+    return mc
 end
 
 function mc_from_inxml(inxml::AbstractString, initconf)
-    @assert isfile(inxml)
-    p = Params()
-    p.output_file = "$(inxml[1:end-7]).out.h5.running"
-    xml2parameters!(p, inxml)
-    mc = DQMC(p)
-    init!(mc)
-
+    mc = mc_from_inxml(inxml)
     mc.p.hsfield .= initconf
-    mc
+    return mc
 end
 
-# tests
-include("O3.jl")
 
-# no interactions
-include("freefermions.jl")
+
+
+
+
+
+
+
+
+
+const SUPPRESS_DQMC_INIT_OUTPUT = true
+
+
+@testset "All Tests" begin
+
+    # tests
+    include("O3.jl")
+
+    # no interactions
+    include("freefermions.jl")
+
+end
 
 nothing
